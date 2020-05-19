f11bb16f35b6b94dbf7ebc4f6989cd820df42f9c
Ginger <55453680+gpemec@users.noreply.github.com>
2020-05-18 23:20:29 -0500

    ..
    .

--- javascript.js
+++ javascript.js
@@ -1,13 +1,3 @@
-//var scrollpos = window.scrollY; // window scroll position
-//var wh = window.innerHeight-50; // as soon as element touches bottom with offset event starts
-//var element = document.querySelector(".sec1-dmush1"); //element
-//
-//window.addEventListener('scroll', function(){ 
-//    if(scrollpos > (element.offsetTop - wh)){
-//        element.classList.add("onScroll");
-//    }
-//});
-//
 
 function getRemainingTime(endTime) {
     var t = Date.parse(endTime) - Date.parse(new Date());
@@ -51,38 +41,31 @@ function initializeClock(id, endTime) {
 var deadline = new Date("Sep 05, 2020 13:00:00");
 initializeClock("clock-divi", deadline);
 
-
 $(document).ready(function() {
-    $('.js--features').waypoint(
-    function(direction) {
-        if (direction == 'down' && screen.width <= '737' ) {
+    $('.js--features').waypoint(function(direction) {
+        if (direction == 'down' && screen.width <= '780' ) {
             $('nav').addClass('sticky');
         } else {
             $('nav').removeClass('sticky');
         }
-   }, {
-       
-//       luego ajustarlo
-    offset: "120px"
+   }, {  offset: "120px"
   });
 
-    
-     $(".js--nav-icon").click(function() {
+    $(".js--nav-icon").click(function() {
     var nav = $(".js--main-nav");
-    var icon = $(".js--nav-icon i");
-
     nav.slideToggle(200);
-
+    var icon = $(".js--nav-icon i");
+        
       if (icon.hasClass("ion-md-menu")) {
         icon.addClass("ion-md-close");
         icon.removeClass("ion-md-menu");
+//          $(".js--main-nav").slideDown();
       } else {
         icon.addClass("ion-md-menu");
         icon.removeClass("ion-md-close");
       }
-
   });
-
-    
 });
+
+
 
